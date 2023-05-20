96ca9d249d186519712dac90b371efc5b8047f65
AnhNhut2639 <zoro92211@gmail.com>
2023-05-20 15:46:44 +0700

    add page orders

--- src/pages/Orders/index.tsx
+++ src/pages/Orders/index.tsx
@@ -1,10 +1,56 @@
-import { Typography } from "antd";
+import { Avatar, Rate, Space, Table, Typography } from "antd";
+import { useEffect, useState } from "react";
+import { getOrders } from "../../apis";
 
 const Orders = () => {
+  const [loading, setLoading] = useState<boolean>(false);
+  const [dataSource, setdataSource] = useState<any>([]);
+  useEffect(() => {
+    setLoading(true);
+    try {
+      getOrders().then((result: any) => {
+        setdataSource(result.products);
+        setLoading(false);
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
   return (
-    <div>
+    <Space size={20} direction="vertical">
       <Typography.Title level={4}>Orders</Typography.Title>
-    </div>
+      <Table
+        columns={[
+          {
+            title: "Title",
+            dataIndex: "title",
+          },
+          {
+            title: "Price",
+            dataIndex: "price",
+            render: (value) => <span>${value}</span>,
+          },
+          {
+            title: "Quantity",
+            dataIndex: "quantity",
+          },
+          {
+            title: "DiscountedPrice",
+            dataIndex: "discountedPrice",
+            render: (value) => <span>${value}</span>,
+          },
+          {
+            title: "Total",
+            dataIndex: "total",
+          },
+        ]}
+        dataSource={dataSource}
+        pagination={{
+          pageSize: 10,
+        }}
+        loading={loading}
+      />
+    </Space>
   );
 };
 export default Orders;
